972f56962c13285effed5a1afa7145adcd6f69eb
Bhavika Badjate <64325998+bhavikabadjate901@users.noreply.github.com>
2021-09-01 10:58:08 +0530

    added working code

--- index.js
+++ index.js
@@ -1,49 +1,21 @@
-const express = require('express');
-const app = express();
-const blog=require('./routes/blog-route')
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/blog2'
 
-const mongoose=require('mongoose')
+const app = express()
 
-var cors = require('cors');
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
 
-app.options('*', cors()); 
+con.on('open', () => {
+    console.log('connected...')
+})
 
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
-    next();
-});
+app.use(express.json())
 
-var whitelist = ['*']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+const alienRouter = require('./routes/blog-route')
+app.use('/aliens',alienRouter)
 
-
-mongoose.connect('mongodb://localhost:27017/myshinynewdb')
-	.then(()=> console.log("connected to Database"))
-  .catch(err=> console.error("Could not connect to mongo db."+ err))
-
-  //MiddleWare for program-> all the api and route are mapped here
-app.use(cors()); 
-app.use(express.json());
-
-
-app.use('/blog', blog);
-
-const Port = process.env.PORT || 3000;
-app.listen(Port,()=> {
-	console.log(`Listening Dude Feel Safe at ${Port}`);
-	// (async function(){
-	// const endpoint= await ngrok.connect(Port);
-	// 	console.log(' ${endpoint}' )
-	// })
-});
-
+app.listen(9000, () => {
+    console.log('Server started')
+})
--- routes/blog-route.js
+++ routes/blog-route.js
@@ -1,98 +1,54 @@
-const express=require('express')
-const router=express.Router()
-const {blogRegistration}=require("../modules/blog-models")
+const express = require('express')
+const router = express.Router()
+const Alien = require('../modules/blog-models')
 
-router.post('/registration',async(req,res)=>{
 
-    // const {error}=validateblog_Registration(req.body)
-    // if (error) return res.status(400).send(`Bad Request ${error}`)
-    
-    // let data= blogRegistration({
-    //     blogID:req.body.blogID,
-    //     uniqueID:req.body.uniqueID,
-    //     title:req.body.title,
-    //     desc:req.body.desc,
-    //     markdown:req.body.markdown,
-    //     photo:req.body.photo,
-    //     auther:req.body.auther,
-    //     createdAt:req.body.createdAt,
-    // })
-    
-    // data= await data.save();
-    // res.json({"message":"Blog Created!!!",
-    //             "status":"200"
-    // })
+router.get('/', async(req,res) => {
+    try{
+           const aliens = await Alien.find()
+           res.json(aliens)
+    }catch(err){
+        res.send({'Error':err})
+    }
+})
 
-        const newPost = new blogRegistration(req.body);
-        try {
-            const savedPost = await newPost.save();
-            res.status(200).json(savedPost);
-        } catch (err) {
-            res.status(500).json(err);
-        }
+router.get('/:id', async(req,res) => {
+    try{
+           const alien = await Alien.findById(req.params.id)
+           res.json(alien)
+    }catch(err){
+        res.send({'Error':err})
+    }
 })
 
 
-router.get('/viewAllBlogs',async(req,res)=>{
-	try{
-	   const posts=await blogRegistration.find();
-	   res.json(posts);
-	}catch(err){
-		res.json({message:err});
-	}
-});
+router.post('/', async(req,res) => {
+    const alien = new Alien({
+        blogID: req.body.blogID,
+        uniqueID: req.body.uniqueID,
+        title: req.body.title,
+        auther: req.body.auther,
+        desc: req.body.desc,
+        markdown: req.body.markdown
+    })
+    try{
+        const a1 =  await alien.save() 
+        res.json(a1)
+    }catch(err){
+        res.send({'Error':err})
+    }
+})
 
-router.get('/viewBlog/:blogID',async(req,res)=>{
-	try{
-	   const posts=await blogRegistration.findOne({blogID:req.params.blogID});
-	   res.json(posts);
-	}catch(err){
-		res.json({message:err});
-	}
-});
-
-router.put('/editBlog/:blogID',async(req,res)=>{
-        const { error }= validateblog_updateRegistration(req.body)
-        if(error) return res.status(400).send(`Bad Request ${error}`)
-    
-        const temp=await blogRegistration.findOneAndUpdate({blogID:req.params.blogID},
-            {$set:req.body},{new:true});
-        if(!temp) return res.status(400).send(`bad Request ${num} not found`);
-    
-        res.json({"message":"successfully update!!!",
-        "status":"200"
-        })
-    
+router.patch('/:id',async(req,res)=> {
+    try{
+        const alien = await Alien.findById(req.params.id) 
+        alien.auther = req.body.auther
+        const a1 = await alien.save()
+        res.json(a1)   
+    }catch(err){
+        res.send({'Error':err})
+    }
 
 })
 
-router.delete('/delete/:blogID',async(req,res)=>{
-	try{
-	 const posts = await blogRegistration.remove({blogID:req.params.blogID});
-	 res.json("Post deleted sucessfully!!!!");
-	}catch(err){
-		res.json({message:err});
-	}
-
-    // try {
-    //     const post = await Post.findById(req.params.id);
-    //     if (post.username === req.body.username) {
-    //       try {
-    //         await post.delete();
-    //         res.status(200).json("Post has been deleted...");
-    //       } catch (err) {
-    //         res.status(500).json(err);
-    //       }
-    //     } else {
-    //       res.status(401).json("You can delete only your post!");
-    //     }
-    //   } catch (err) {
-    //     res.status(500).json(err);
-    //   }
-
-
-});
-
-module.exports=router
-
-
+module.exports = router
